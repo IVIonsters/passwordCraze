481ef604a33c018803546c6b495d66c1b6f2282e
Zachary Polof <ZacharyPolof@gmail.com>
2024-04-21 21:20:21 -0500

    adding JS file
    added script.js file
    linked to html
    added variables for generator

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// defined variables for password generator
+const passwordElement = document.getElementById('password');
+const length = 14;
+
+// added variables for password characters
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const special = '!@#$%^&*';
