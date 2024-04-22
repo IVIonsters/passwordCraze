6e33edaf78bdb225a33493762475c8e065999dc2
Zachary Polof <ZacharyPolof@gmail.com>
2024-04-21 21:26:30 -0500

    Update script.js
    added function to generate password using provided variables
    
    added additional variable to for all characters
    
    added while loop to continue to add characters until desired length is achieved

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,19 @@ const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 const numbers = '0123456789';
 const special = '!@#$%^&*';
+
+const addCharacters = upperCase + lowerCase + numbers + special;
+
+// function to generate password
+function generatePassword() {
+    let password = "";
+    // math.random to generate password, math.floor to round down
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += special[Math.floor(Math.random() * special.length)];
+
+    while (length > password.length) {
+        password += addCharacters[Math.floor(Math.random() * addCharacters.length)];
+    }
+}
